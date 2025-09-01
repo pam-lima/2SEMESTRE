b8a721ec701e8624c31dad63ae7f0d657fbbed34
unknown <lab1053@etecjk.local>
2025-09-01 20:32:22 -0300

    Alterações aula 3 php

--- PW2/php_aula3/calculadora.php
+++ PW2/php_aula3/calculadora.php
@@ -1,24 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Calculadora</title>
 </head>
 <body>
     <form action="resultado.php" method="post">
-        <pre>
         <Label for="Digite o primeiro número">Numero 1:</Label>
         <input type="number" name="n1"> 
         <label for="Digite o segundo número">Numero 2:</label>
         <input type="number" name="n2">
-        <select name="opeacao">
+        <select value="operacao" name="operacao">
         <option value="soma">+</option>
         <option value="subtrair">-</option>
         <option value="multiplicar">x</option>
         <option value="dividir">/</option>
-        <button for="calculo">Calcular</button>
-        <input type="submit" value="calcular"> <input type="reset" value="limpar">
+        <input type="submit" value="calcular">
+        <input type="reset" value="limpar">
     </form>
 </body>
 </html>
--- PW2/php_aula3/resultado.php
+++ PW2/php_aula3/resultado.php
@@ -1,26 +1,42 @@
 <?php
         $n1 = $_POST["n1"];
         $n2 = $_POST["n2"];
+        $operacao = $_POST["operacao"];
+
         function soma ($n1, $n2){
-            return $n1 + $n2;
+            $resultado = $n1 + $n2;
+            return $resultado;
         }
 
         function subtrair ($n1, $n2){
-            return $n1 - $n2;
+            $resultado = $n1 - $n2;
+            return $resultado;
         }
 
         function multiplicar ($n1, $n2){
-            return $n1 * $n2;
+            $resultado = $n1 * $n2;
+            return $resultado;
         }
 
         function dividir ($n1, $n2){
-            return $n1 / $n2;
+            $resultado = $n1 / $n2;
+            return $resultado;
+            if($n2 == 0){
+                return "Operação inválida";
+            }
+            
         } 
-
-        echo "O resultado da soma é: " .$_POST(soma);
-        echo "O resultado da subtração é: " .$_POST(subtrair);
-        echo "O resultado da multiplicação é: " .$_POST(multiplicar);
-        echo "O resultado da divisão é: " .$_POST(divir);
+        if($operacao == "soma"){
+            echo "O resultado da soma é: " .soma($n1, $n2);
+        } elseif($operacao == "subtrair"){
+            echo "O resultado da subtração é: " .subtrair($n1, $n2);
+        } elseif($operacao == "multiplicar"){
+            echo "O resultado da multiplicação é: " .multiplicar($n1, $n2);
+        } elseif($operacao == "dividir"){
+            echo "O resultado da divisão é: " .dividir($n1, $n2);
+        } else {
+            echo "Operação inválida!";
+        }
         ?>
 
         
