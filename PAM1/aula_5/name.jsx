32c14af39a9f351a00464ad2f4ca7bd2366e7039
Pamela <pamela.santos326@etec.sp.gov.br>
2025-09-18 12:39:31 -0300

    Adicionada aula 5 11/09 react native typescript

---
+++ PAM1/aula_5/name.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text, View} from "react native";
+
+export default function Name() {
+    let name = "Pamela";
+    return (
+        <View>
+            <Text>{name}</Text>
+        </View>
+    );
+}
