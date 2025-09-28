d2d5c1551fbc49d24e5ed3c03df047712d358369
Pamela <pamela.santos326@etec.sp.gov.br>
2025-09-28 18:21:22 -0300

    aula 5 PAM1

--- PAM1/aula_5/name1.tsx
+++ PAM1/aula_5/name1.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, View } from "react-native";
 
 export default function Name() {
-    let name = "Pamela";
+    const name = "Pamela";
     return (
         <View>
             <Text>{name}</Text>
